2742ea97ba4f34991ada67fd25aae5cbf03eb739
YnsOzt <ozturk213@hotmail.fr>
2019-03-07 11:09:27 +0100

    doc: added some documentation on the methods

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,24 @@
 
 public class Graph {
 
+  /**
+   * method that calculate the shortest path.
+   * 
+   * @param actorSrc The source actor
+   * @param actorDest The destination actor
+   * @param file the file to parse
+   */
   public void calculerCheminLePlusCourt(String actorSrc, String actorDest, String file) {
 
   }
 
+  /**
+   * method that calculate the lowest cost path
+   * 
+   * @param actorSrc The source actor
+   * @param actorDest The destination actor
+   * @param file the file to parse
+   */
   public void calculerCheminCoutMinimum(String actorSrc, String actorDest, String file) {
 
   }
--- src/SAXHandler.java
+++ src/SAXHandler.java
@@ -2,6 +2,10 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXHandler extends DefaultHandler {
 
+  /**
+   * 
+   * @return a graph build by the handler
+   */
   public Graph getGraph() {
     return null;
   }

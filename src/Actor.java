11c73f8811118eccb6d8a40abe0b526916fc01b8
vinztheprinze <44948197+vinztheprinze@users.noreply.github.com>
2019-03-07 11:22:38 +0100

    Create : Class Actor

---
+++ src/Actor.java
@@ -0,0 +1,49 @@
+
+public class Actor {
+	
+	private String id;
+	private String name;
+	
+	
+	public Actor(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+		
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Actor other = (Actor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Actor [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}
